da080cb4c7880f8f6adb5618a590acd1572d4371
natsuya yamashiro <azuki728@me.com>
2019-10-14 20:51:45 +0900

    PinBallの課題です。得点表示まで。

---
+++ Assets/ScoreText.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+	public GameObject scoretext;
+	private int score = 0; //スコア計算用変数
+
+	void Start()
+	{
+		score   = 0;
+		SetScore();   //初期スコアを代入して表示
+	}
+		
+	void OnCollisionEnter( Collision collision )
+	{
+
+		string Tag  = collision.gameObject.tag;
+
+		if (Tag == "SmallStarTag")
+		{
+			score += 20;
+		} 
+
+		else if (Tag == "LargeStarTag")
+		{
+			score += 10;
+		}
+
+		else if(Tag == "SmallCloudTag")
+		{
+			score += 5;
+		}
+
+		else if(Tag == "LargeCloudTag")
+		{
+			score += 1;
+		}
+
+		SetScore();
+	}
+
+	void SetScore()
+	{
+		this.scoretext = GameObject.Find("ScoreText");
+
+		this.scoretext.GetComponent<Text> ().text = score + "point";
+	}
+
+}
